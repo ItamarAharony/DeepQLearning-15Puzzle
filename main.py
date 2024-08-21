31e106e98da6830c980f542660c0157745993fc2
ItamarAharony <64987789+ItamarAharony@users.noreply.github.com>
2024-08-21 15:28:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from .training import train_dqn
+from training import train_dqn
 
 if __name__ == "__main__":
     train_dqn(episodes=100, num_shuffles=0, max_steps_per_episode=10, epsilon=0.05, epsilon_decay=1, learning_rate=0.1, resume_training=False, filename="dqn_15_puzzle.weights.h5")
