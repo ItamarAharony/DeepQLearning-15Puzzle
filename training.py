f975b26882d4e9c4ba0acdf57b0c446a4b0358d3
ItamarAharony <64987789+ItamarAharony@users.noreply.github.com>
2024-08-21 15:23:13 -0400

    Update training.py

--- training.py
+++ training.py
@@ -71,4 +71,11 @@ def train_dqn(episodes, num_shuffles=1, max_steps_per_episode=10, epsilon=1.0, e
     agent.save(filename)
 
 # Train the DQN agent with the specified parameters
+"""
+The file to which the Deep-Q Network weights and biases will be saved is filename="dqn_15_puzzle.weights.h5".
+If you are running this code while a file called dqn_15_puzzle.weights.h5 does not exist in this program's directory, keep resume_training=False and
+the h5 file will be generated.
+Otherwise if dqn_15_puzzle.weights.h5 exists, resume_training=False will overwrite the file while resume_training=True will resume the training from the saved model
+in dqn_15_puzzle.weights.h5.
+"""
 train_dqn(episodes=100, num_shuffles=0, max_steps_per_episode=10, epsilon=0.05, epsilon_decay=1, learning_rate=0.1, resume_training=False, filename="dqn_15_puzzle.weights.h5")
